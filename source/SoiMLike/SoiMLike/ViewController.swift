ab9e504cc673d79f848ba52a46798349299b680c
GDMiao <miao_guodong@sina.com>
2020-05-25 00:12:43 +0800

    Soi M Like

---
+++ source/SoiMLike/SoiMLike/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SoiMLike
+//
+//  Created by Guodong Miao on 2020/5/25.
+//  Copyright © 2020 Guodong Miao. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
